1051bbbbb251a8e6805cac37afdcf99622fdb08a
Sergey Potapov <blake131313@gmail.com>
2023-08-06 11:28:17 +0200

    Add Kind trait

--- kinded/src/traits.rs
+++ kinded/src/traits.rs
@@ -3,7 +3,13 @@ use ::core::fmt::Debug;
 /// A trait that can be implemented by a main enum type.
 /// Typically should be derived with `#[derive(kinded::Kinded)]`.
 pub trait Kinded {
-    type Kind: PartialEq + Eq + Debug + Clone + Copy;
+    type Kind: PartialEq + Eq + Debug + Clone + Copy + Kind;
 
+    /// Get a kind variant without data.
     fn kind(&self) -> Self::Kind;
+}
+
+pub trait Kind: PartialEq + Eq + Debug + Clone + Copy {
+    /// Return a vector with all possible kind variants.
+    fn all() -> Vec<Self>;
 }
